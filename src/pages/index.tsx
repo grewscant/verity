780d9f03d4883ecbd3fbba1be7c156239bfa708a
Danyl Fernandes <realllydan@gmail.com>
2022-03-09 21:00:19 +0530

    :sparkles: Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,67 @@
+import styled from "styled-components";
 import type { NextPage } from "next";
 import { Navbar } from "../components/Navbar";
+import Image from "next/image";
+
+const MainContainer = styled.div(
+  ({ theme }) => `
+    width: 100vw;
+    height: 100vh;
+    background-color: ${theme.colors["purple-0"]};
+  `
+);
+
+const Flex = styled.div`
+  display: flex;
+`;
+
+const Heading = styled.h1(
+  ({ theme }) => `
+    margin: 0;
+    width: 600px;
+    font-weight: ${theme.fontWeights.extrabold};
+    font-size: 72px;
+    line-height: 72px;
+  `
+);
+
+const SubHeading = styled.h3(
+  ({ theme }) => `
+    margin: 0;
+    margin-top: 20px;
+    width: 800px;
+    font-weight: ${theme.fontWeights.semibold};
+    font-size: 52px;
+    line-height: 55px;
+  `
+);
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <MainContainer>
       <Navbar title="V" />
-    </div>
+      <Flex
+        style={{
+          height: "70%",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "48px",
+        }}
+      >
+        <Flex style={{ height: "max-content", flexDirection: "column" }}>
+          <Heading>Sneaker buying Re-invented</Heading>
+          <SubHeading>
+            Verity allows you to buy sneakers as ERC721 NFT-tokens with $ETH.{" "}
+          </SubHeading>
+        </Flex>
+        <Image
+          src="/homepage-shoe.png"
+          alt="Nike Air Jordan"
+          width={1000}
+          height={600}
+        />
+      </Flex>
+    </MainContainer>
   );
 };
 
