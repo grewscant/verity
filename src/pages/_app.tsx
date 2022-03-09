164b048ef089e07ddef61ee094015dce8b6c3a97
Danyl Fernandes <realllydan@gmail.com>
2022-03-09 19:11:35 +0530

    :lipstick: Add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
 import { ThemeProvider } from "../components/ThemeProvider";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Nunito Sans';
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
   );
